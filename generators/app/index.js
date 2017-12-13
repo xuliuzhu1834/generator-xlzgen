cb81db4ad8133b5cec069f6ab307cd0d1b506d85
xuliuzhu <xuliuzhu@shein.com>
2017-12-13 16:30:11 +0800

    em .... bug everywhere

---
+++ generators/app/index.js
@@ -0,0 +1,51 @@
+
+const Gen = require('yeoman-generator');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const tpl = require('./templates/tpl');
+
+module.exports = class extends Gen {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+  prompting () {
+    return this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: '请输入文件夹名称',
+    }]).then((res) => {
+      const path = this.destinationPath(res.name);
+      console.log(path,'path');
+
+      fs.exists(path, (e) => {
+        if (!e) {
+          // 创建目标文件夹
+          mkdirp(res.name, (err) => (!!err && this.log(err)));
+
+          //目标文件夹下创建 list 文件夹
+          mkdirp(`${path}/list`, (err) => (!!err && this.log(err)));
+
+          //目标文件夹下创建 add 文件夹
+          mkdirp(`${path}/add`, (err) => (!!err && this.log(err)));
+
+          //目标文件夹下创建 edit 文件夹
+          mkdirp(`${path}/edit`, (err) => (!!err && this.log(err)));
+
+          //目标文件夹下创建 server 文件
+          const viewPath = path.replace(path.split(/components|component/)[0], '').split('/').slice(1).join('/');
+          this.fs.write(`${path}/server.js`, tpl.server());
+          ['list', 'add', 'edit'].forEach((v) => {
+            this.fs.write(`${path}/${v}/view.jsx`, tpl[v](res.name, `'${viewPath}/${v}'`));
+            this.fs.write(`${path}/${v}/action.js`, tpl.action());
+            this.fs.write(`${path}/${v}/types.js`, tpl.types());
+            this.fs.write(`${path}/${v}/saga.js`, tpl.saga());
+            this.fs.write(`${path}/${v}/reducers.js`, tpl.reducer());
+          })
+        } else {
+          this.log('该文件夹已经存在');
+        }
+      });
+    })
+  }
+
+};
---
+++ generators/app/templates/tpl.js
@@ -0,0 +1,114 @@
+const list = (name, path) => (`
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class ${name}List extends Component {
+  constructor(props) {
+      super(props);
+      
+  }
+  render() {
+    return (<div></div>)
+  }
+}
+
+${name}List.propTypes = {
+ 
+};
+const mapStateToProps = state => state[${path}];
+export default connect(mapStateToProps)(${name}List);
+`);
+
+const add = (name, path) => (`
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class ${name}Add extends Component {
+  constructor(props) {
+      super(props);
+      
+  }
+  render() {
+    return (<div></div>)
+  }
+}
+
+${name}Add.propTypes = {
+ 
+};
+const mapStateToProps = state => state[${path}];
+export default connect(mapStateToProps)(${name}Add);
+`);
+
+const edit = (name, path) => (`
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class ${name}edit extends Component {
+  constructor(props) {
+      super(props);
+      
+  }
+  render() {
+    return (<div></div>)
+  }
+}
+
+${name}Add.propTypes = {
+ 
+};
+const mapStateToProps = state => state[${path}];
+export default connect(mapStateToProps)(${name}edit);
+`);
+
+const reducer = () => (`
+import assign from 'object-assign';
+import * as types from './types';
+export const defaultState = {};
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case types.commit:
+      return assign({}, state, {
+        [action.key]: action.value,
+      });
+    default:
+      return state;
+  }
+}
+export default reducer;
+`);
+
+const action = () => (`
+import * as types from './types';
+
+export const commit = (key, value) => ({ types.commit, key, value });
+`);
+
+const saga = () => (`
+import { message } from 'antd';
+import { takeLatest, takeEvery } from 'redux-saga/effects';
+
+function* saga() {
+
+}
+export default saga;
+`);
+const types = () => (`
+export let commit;
+`);
+const style = () => ('');
+const server = (path) => ('');
+module.exports = {
+  list,
+  add,
+  edit,
+  action,
+  saga,
+  reducer,
+  server,
+  types,
+};
